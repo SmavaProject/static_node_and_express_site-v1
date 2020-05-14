a856a7b025547bf85b0db33dd180d5485576c32f
SmavaProject <katyakravchenka@yahoo.com>
2020-05-13 22:12:52 -0700

    project setup

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const data = require('./data.json'); //path!!!
+const app = express();
+//Optionally - the path module which can be used when setting the absolute path in the express.static function.
