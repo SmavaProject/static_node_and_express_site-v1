87ef80e72120c5fa6c5e7c410c51e9db011a74b1
SmavaProject <katyakravchenka@yahoo.com>
2020-05-16 21:49:23 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const data = require('./data.json'); //path!!!
 const app = express();
-//Optionally - the path module which can be used when setting the absolute path in the express.static function.
-
 
 app.use('/static', express.static('public'));
 
@@ -11,21 +9,12 @@ const indexRoute = require('./routes'); //An "index" route (/) to render the "Ho
 app.use(indexRoute);
 const about = require('./routes/about.js');
 app.use('/about', about);
-/***
- * Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version of
- * the Pug project template to show off each project.
- * Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
- */
+
 const project = require('./routes/project.js');
 app.use('/project', project);
 
 app.set('view engine', 'pug');
 
-
-
-//Refer to the video on Error handling Middleware, which is linked in the project resources list.
-
-//function which catches errors and passes them to the error handler
 app.use((req,res, next) =>{
     const err = new Error('Page Not Found... Please try something else');
     err.status = 404;
@@ -37,7 +26,6 @@ app.use((err, req, res, next) => {
     res.status(err.status);
     res.render('error');
 });
-
 
 
 app.listen(3000, ()=> {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-//const {projects} = require('../data.json');
 const {projects} = require('../data.json');
 
 router.get('/', (request, response)=>{
 
     response.render('index', {projects});
 });
-
-
 
 
 module.exports = router;
